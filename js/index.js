d31c063bdafc4e9ded0e03fcb01b029305e57c04
Adam777 <Adam777-5867@outlook.com>
2019-10-25 15:34:33 +0000

    Update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 const projectName = 'random-quote-machine';
 localStorage.setItem('example_project', 'Random Quote Machine');
 
+var quotes;
+
 $(document).ready(function() {
-	getQuote();
+	$.getJSON('https://adam777z.github.io/projects/random-quote-machine/json/quotesondesign.json', function(data) {
+		quotes = data;
+
+		getQuote();
+	});
 
 	$('#new-quote').click(function() {
 		getQuote();
@@ -10,13 +16,16 @@ $(document).ready(function() {
 });
 
 function getQuote() {
-	$.getJSON('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&per_page=1&_=' + $.now(), function(data) {
-		var quote = $.trim($($.parseHTML(data[0]['content']['rendered'])).text());
-		var author = data[0]['title']['rendered'];
-		var tweetText = '"' + quote + '" - ' + author;
+	var i = randomIntFromInterval(0, quotes.length);
+	var quote = $.trim($($.parseHTML(quotes[i]['content']['rendered'])).text());
+	var author = quotes[i]['title']['rendered'];
+	var tweetText = '"' + quote + '" - ' + author;
 
-		$('blockquote p').html(quote);
-		$('blockquote footer cite').html(author).attr('title', author);
-		$('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweetText));
-	});
+	$('blockquote p').html(quote);
+	$('blockquote footer cite').html(author).attr('title', author);
+	$('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweetText));
+}
+
+function randomIntFromInterval(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
